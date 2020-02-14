52cdabb25f49072ea7b6a82d4fcf585cfd6279d0
Harsha Chamrajnagar Srikara <hxc170009@cslinux1.utdallas.edu>
2020-02-13 22:45:56 -0600

    working version

--- assignment2/main.cc
+++ assignment2/main.cc
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <algorithm>
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 #include "parser.h"
 
 std::string readFile(std::string infile);
 void writeFile(std::string outfile, std::string text);
+std::string uppercase(std::string text);
+std::string lowercase(std::string text);
 
 int main(int argc, char *argv[])
 {
     parser commandLineParser;
     commandLineParser.parseCommandLine(argc, argv);
     std::map<key,std::string> map = commandLineParser.getMap();
-    readFile(map[infile]);
+    std::string data = readFile(map[infile]);
+    writeFile(map[outfile], data);
+    std::string rando = uppercase(data);
     return 0;
 }
 
 std::string readFile(std::string infile) {
     std::ifstream myInputFile;
-    myInputFile.open(infile);
+    myInputFile.open(infile.c_str(), std::ios::in);
     std::string temp;
     std::string outputString;
     if(myInputFile) {
@@ -44,7 +48,7 @@ std::string lowercase(std::string text) {
 
 void writeFile(std::string outfile, std::string text) {
     std::ofstream myOutputFile;
-    myOutputFile.open(outfile);
+    myOutputFile.open(outfile.c_str(), std::ios::out);
     myOutputFile << text << std::endl;
     myOutputFile.close();
 }
